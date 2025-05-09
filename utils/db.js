e60ff323c3a0f63335f5af0fbedf7a3638f29138
Fajar Setyawan <106090316+crackspace770@users.noreply.github.com>
2025-05-09 14:02:08 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2';
 
 export const db = mysql.createConnection({
   host: 'localhost',       // or your remote DB host
-  user: 'root',   // e.g., 'root'
-  password: 'admin',
+  user: '',   // e.g., 'root'
+  password: '',
   database: 'node_api_db'
 });
 
